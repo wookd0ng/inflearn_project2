cb94327d5cb878a358aea5b94ad6cfaa79b0895e
wookd0ng <100339436+wookd0ng@users.noreply.github.com>
2023-08-05 16:43:23 +0900

    Add files via upload

---
+++ class/03-javascript/01-variable.js
@@ -0,0 +1,3 @@
+const name="홍길동"
+name="스티브 잡스"
+console.log(name)
---
+++ class/03-javascript/02-object-array.js
@@ -0,0 +1,212 @@
+// const fruits=[
+//     {number: 1, title: "레드향" },
+//     {number: 2, title: "샤인머스켓" },
+//     {number: 3, title: "산청딸기" },
+//     {number: 4, title: "한라봉" },
+//     {number: 5, title: "사과" },
+//     {number: 6, title: "애플망고" },
+//     {number: 7, title: "딸기" },
+//     {number: 8, title: "천혜향"},
+//     {number: 9, title: "과일선물세트"},
+//     {number: 10, title: "귤"},
+        
+// ]
+// // undefined
+// fruits
+// // (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]0: {number: 1, title: '레드향'}1: {number: 2, title: '샤인머스켓'}2: {number: 3, title: '산청딸기'}3: {number: 4, title: '한라봉'}4: {number: 5, title: '사과'}5: {number: 6, title: '애플망고'}6: {number: 7, title: '딸기'}7: {number: 8, title: '천혜향'}8: {number: 9, title: '과일선물세트'}9: {number: 10, title: '귤'}length: 10[[Prototype]]: Array(0)
+// fruits.number
+// // undefined
+// fruits.number=fruits[0]
+// // {number: 1, title: '레드향'}
+// let name=[]
+// // undefined
+// fruits[0].number
+// // 1
+// fruits[0].title
+// // '레드향'
+// fruits[0].number + " " +fruits[0].title
+// // '1 레드향'
+// const fruits=[
+//     {number: 1, title: "레드향" },
+//     {number: 2, title: "샤인머스켓" },
+//     {number: 3, title: "산청딸기" },
+//     {number: 4, title: "한라봉" },
+//     {number: 5, title: "사과" },
+//     {number: 6, title: "애플망고" },
+//     {number: 7, title: "딸기" },
+//     {number: 8, title: "천혜향},    
+        
+// ]
+// VM5325:9 Uncaught SyntaxError: Invalid or unexpected token
+// const fruits=[
+//     {number: 1, title: "레드향" },
+//     {number: 2, title: "샤인머스켓" },
+//     {number: 3, title: "산청딸기" },
+//     {number: 4, title: "한라봉" },
+//     {number: 5, title: "사과" },
+//     {number: 6, title: "애플망고" },
+//     {number: 7, title: "딸기" },
+//     {number: 8, title: "천혜향"},
+//     {number: 9, title: "과일선물세트"},
+//     {number: 10, title: "귤"},
+        
+// ]
+// undefined
+// fruits
+// (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]0: {number: 1, title: '레드향'}1: {number: 2, title: '샤인머스켓'}2: {number: 3, title: '산청딸기'}3: {number: 4, title: '한라봉'}4: {number: 5, title: '사과'}5: {number: 6, title: '애플망고'}6: {number: 7, title: '딸기'}7: {number: 8, title: '천혜향'}8: {number: 9, title: '과일선물세트'}9: {number: 10, title: '귤'}length: 10[[Prototype]]: Array(0)
+// fruits.number
+// undefined
+// fruits.number=fruits[0]
+// {number: 1, title: '레드향'}
+// let name=[]
+// undefined
+// fruits[0].number
+// 1
+// fruits[0].title
+// '레드향'
+// fruits[0].number + " " +fruits[0].title
+// '1 레드향'
+// fruits[0].number + " " +fruits[0].title
+// fruits[1].number + " " +fruits[1].title
+// fruits[2].number + " " +fruits[2].title
+// fruits[3].number + " " +fruits[3].title
+// fruits[4].number + " " +fruits[4].title
+// fruits[5].number + " " +fruits[5].title
+// fruits[6].number + " " +fruits[6].title
+// fruits[7].number + " " +fruits[7].title
+// fruits[8].number + " " +fruits[8].title
+// fruits[9].number + " " +fruits[9].title
+// fruits[10].number + " " +fruits[10].title
+
+// VM5843:11 Uncaught TypeError: Cannot read properties of undefined (reading 'number')
+//     at <anonymous>:11:12
+// (익명) @ VM5843:11
+// let result=[]
+// undefined
+// result=[
+//     {fruits[0].number + " " +fruits[0].title},
+//     {fruits[0].number + " " +fruits[0].title},
+    
+
+// ]
+// VM5934:2 Uncaught SyntaxError: Unexpected token '['
+// result=[
+//     {fruits[0].number + " " +fruits[0].title},
+//     {fruits[1].number + " " +fruits[1].title},
+//     {fruits[2].number + " " +fruits[2].title},
+//     {fruits[3].number + " " +fruits[3].title},
+//     {fruits[4].number + " " +fruits[4].title},
+//     {fruits[5].number + " " +fruits[5].title},
+//     {fruits[6].number + " " +fruits[6].title},
+//     {fruits[7].number + " " +fruits[7].title},
+//     {fruits[8].number + " " +fruits[8].title},
+//     {fruits[9].number + " " +fruits[9].title},
+
+// ]
+// VM6014:2 Uncaught SyntaxError: Unexpected token '['
+// const real=[
+//     {fruits[0].number + " " +fruits[0].title},
+//     {fruits[1].number + " " +fruits[1].title},
+//     {fruits[2].number + " " +fruits[2].title},
+//     {fruits[3].number + " " +fruits[3].title},
+//     {fruits[4].number + " " +fruits[4].title},
+//     {fruits[5].number + " " +fruits[5].title},
+//     {fruits[6].number + " " +fruits[6].title},
+//     {fruits[7].number + " " +fruits[7].title},
+//     {fruits[8].number + " " +fruits[8].title},
+//     {fruits[9].number + " " +fruits[9].title},
+
+// ]
+// VM6043:2 Uncaught SyntaxError: Unexpected token '['
+// const goal=fruits[0].number + " " +fruits[0].title
+// undefined
+// goal
+// '1 레드향'
+// const second=[]
+// undefined
+// second=
+//     {fruits[0].number + " " +fruits[0].title},
+//     {fruits[1].number + " " +fruits[1].title},
+//     {fruits[2].number + " " +fruits[2].title},
+//     {fruits[3].number + " " +fruits[3].title},
+//     {fruits[4].number + " " +fruits[4].title},
+//     {fruits[5].number + " " +fruits[5].title},
+//     {fruits[6].number + " " +fruits[6].title},
+//     {fruits[7].number + " " +fruits[7].title},
+//     {fruits[8].number + " " +fruits[8].title},
+//     {fruits[9].number + " " +fruits[9].title},
+
+// VM6158:2 Uncaught SyntaxError: Unexpected token '['
+// second=
+//     {fruits[0].number + " " +fruits[0].title},
+//     {fruits[1].number + " " +fruits[1].title},
+//     {fruits[2].number + " " +fruits[2].title},
+//     {fruits[3].number + " " +fruits[3].title},
+//     {fruits[4].number + " " +fruits[4].title},
+//     {fruits[5].number + " " +fruits[5].title},
+//     {fruits[6].number + " " +fruits[6].title},
+//     {fruits[7].number + " " +fruits[7].title},
+//     {fruits[8].number + " " +fruits[8].title},
+//     {fruits[9].number + " " +fruits[9].title},
+// VM6162:2 Uncaught SyntaxError: Unexpected token '['
+// second=[
+//     {fruits[0].number + " " +fruits[0].title},
+//     {fruits[1].number + " " +fruits[1].title},
+//     {fruits[2].number + " " +fruits[2].title},
+//     {fruits[3].number + " " +fruits[3].title},
+//     {fruits[4].number + " " +fruits[4].title},
+//     {fruits[5].number + " " +fruits[5].title},
+//     {fruits[6].number + " " +fruits[6].title},
+//     {fruits[7].number + " " +fruits[7].title},
+//     {fruits[8].number + " " +fruits[8].title},
+//     {fruits[9].number + " " +fruits[9].title},
+// ]
+// VM6172:2 Uncaught SyntaxError: Unexpected token '['
+// let map = fruits[0].number + " " +fruits[0].title
+// undefined
+// map
+// '1 레드향'
+// let map = fruits[0].number + " " +fruits[0].title,fruits[0].number + " " +fruits[0].title
+// VM6226:1 Uncaught SyntaxError: Unexpected token '['
+// let map = [fruits[0].number + " " +fruits[0].title,fruits[0].number + " " +fruits[0].title]
+// undefined
+// map
+// (2) ['1 레드향', '1 레드향']
+// let map = [fruits[0].number + " " +fruits[0].title,
+//            fruits[1].number + " " +fruits[1].title
+//           fruits[0].number + " " +fruits[0].title
+//     fruits[0].number + " " +fruits[0].title
+//         fruits[0].number + " " +fruits[0].title
+//             fruits[0].number + " " +fruits[0].title
+//                 fruits[0].number + " " +fruits[0].title
+//                     fruits[0].number + " " +fruits[0].title
+//                         fruits[0].number + " " +fruits[0].title
+//                             fruits[0].number + " " +fruits[0].title
+//                                 fruits[0].number + " " +fruits[0].title]
+// VM6281:3 Uncaught SyntaxError: Unexpected identifier 'fruits'
+// let map = [fruits[0].number + " " +fruits[0].title,
+//            fruits[1].number + " " +fruits[1].title
+//            fruits[2].number + " " +fruits[2].title
+//            fruits[3].number + " " +fruits[3].title
+//            fruits[4].number + " " +fruits[4].title
+//            fruits[5].number + " " +fruits[5].title
+//            fruits[6].number + " " +fruits[6].title
+//            fruits[7].number + " " +fruits[7].title
+//            fruits[8].number + " " +fruits[8].title
+//            fruits[9].number + " " +fruits[9].title
+//           ]
+// VM6396:3 Uncaught SyntaxError: Unexpected identifier 'fruits'
+// let map = [fruits[0].number + " " +fruits[0].title,
+//            fruits[1].number + " " +fruits[1].title,
+//            fruits[2].number + " " +fruits[2].title,
+//            fruits[3].number + " " +fruits[3].title,
+//            fruits[4].number + " " +fruits[4].title,
+//            fruits[5].number + " " +fruits[5].title,
+//            fruits[6].number + " " +fruits[6].title,
+//            fruits[7].number + " " +fruits[7].title,
+//            fruits[8].number + " " +fruits[8].title,
+//            fruits[9].number + " " +fruits[9].title,
+//           ]
+// undefined
+// map
+// (10) ['1 레드향', '2 샤인머스켓', '3 산청딸기', '4 한라봉', '5 사과', '6 애플망고', '7 딸기', '8 천혜향', '9 과일선물세트', '10 귤']
